aa7d99d654cfe90622cf48b62f603dfa6f3c3d08
Brian <brianmakorious@gmail.com>
2023-02-08 10:51:31 +0300

    added readme

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const display = document.querySelector()
+const display = document.querySelector("display")
+const display = document.querySelectorAll("button")
+
+buttons.array.forEach(element => 
+    {btn.addEvent("click", () =>
+if(btn.id=== "="){
+    display.value = eval(display.value);
+    else if ( btn.id==="ac"){
+        display value ="";
+    }else if (btn.id =="del"){
+        display.value = display.value.slice(0,-1);
+    }else{
+        display.value +=btn.id;
+    }
+    
+    }
+    }
+}
+    
+});
